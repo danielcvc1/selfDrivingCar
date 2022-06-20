46e469c2faa46f311be12c7cb027558986cbf38a
danielcvc1 <82286699+danielcvc1@users.noreply.github.com>
2022-06-20 15:42:05 +0200

    Add files via upload

--- car.js
+++ car.js
@@ -12,9 +12,14 @@ class Car {
         this.angle = 0
         this.damaged = false
 
+        this.useBrain=controlType=="AI"
+
         if (controlType != "DUMMY") {
             this.sensor = new Sensor(this)
-
+            this.brain=new NeuralNetwork(
+                [this.sensor.rayCount,6,4]
+            )
+           
         }
 
 
@@ -27,8 +32,18 @@ class Car {
             this.damaged = this.#assessDamage(roadBorders,traffic)
         }
         if (this.sensor) {
-            this.sensor.update(roadBorders,traffic)
+            this.sensor.update(roadBorders,traffic);
+            const offsets=this.sensor.readings.map(
+                s=>s==null?0:1-s.offset
+            );
+            const outputs=NeuralNetwork.feedForward(offsets,this.brain);
 
+            if(this.useBrain){
+                this.controls.forward=outputs[0]
+                this.controls.left=outputs[1]
+                this.controls.right=outputs[2]
+                this.controls.reverse=outputs[3]
+            }
         }
     }
 
@@ -118,11 +133,11 @@ class Car {
         this.y -= Math.cos(this.angle) * this.speed
     }
 
-    draw(ctx) {
+    draw(ctx,color) {
         if (this.damaged) {
             ctx.fillStyle = "lightgray"
         } else {
-            ctx.fillStyle = "black"
+            ctx.fillStyle = color
         }
         ctx.beginPath()
         ctx.moveTo(this.polygon[0].x, this.polygon[0].y)
